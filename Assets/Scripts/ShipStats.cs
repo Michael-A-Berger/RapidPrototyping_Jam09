245ef9f65fe1fc9fc7053a6c05a7cfbd5c91eb90
rhetley <54686199+rhetley@users.noreply.github.com>
2020-04-04 15:22:19 -0400

    First pass on slicing sprites

---
+++ Assets/Scripts/ShipStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStats : MonoBehaviour
+{
+    public int appearance;
+    public int interior;
+    public int safety;
+    public int speed;
+    public enum SizeCategory { Small, Regular, Large }
+    public SizeCategory size;
+    public enum ShipModifier { None }
+    public ShipModifier modifier = ShipModifier.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
