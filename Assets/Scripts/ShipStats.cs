3b495dca8953cfe3d098f2c6a3bb1509757a36df
rhetley <54686199+rhetley@users.noreply.github.com>
2020-04-04 20:11:17 -0400

    Many ship prefabs

--- Assets/Scripts/ShipStats.cs
+++ Assets/Scripts/ShipStats.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class ShipStats : MonoBehaviour
 {
+    // This would be "name," but that term is reserved
+    public string model;
+    
+    // The five stats for determining the selling price
     public int appearance;
     public int interior;
     public int safety;
     public int speed;
     public enum SizeCategory { Small, Regular, Large }
     public SizeCategory size;
+
+    // Ship modifiers are distinct from customer modifiers
     public enum ShipModifier { None }
     public ShipModifier modifier = ShipModifier.None;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // All values are set in the Inspector
     }
-
-    // Update is called once per frame
-    /*void Update()
-    {
-        
-    }*/
 }
