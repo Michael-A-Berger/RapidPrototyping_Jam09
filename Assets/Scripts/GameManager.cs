dc3757e2f22c055b9a6c375b2acad7b44b5abd23
Lanzarote_ZRY <33143405+LanzaroteZRY@users.noreply.github.com>
2020-04-06 11:46:13 -0400

    Basic UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class GameManager : MonoBehaviour
     public float currentCustomerPatience = 5f;
     public float finalBuyingPrice;
 
+    public Button inter;
+    public Button boa;
+    public Button sna;
+    public Button offer;
  
 
 
