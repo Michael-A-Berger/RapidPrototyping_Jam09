4c2cb507c0a1641d694e11611afd1c25400495c4
Lanzarote_ZRY <33143405+LanzaroteZRY@users.noreply.github.com>
2020-04-06 11:24:58 -0400

    MainLoop
    Just start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager instance;
+
+    public ShipStats currentShip;
+    public CustomerStats currentCustomer;
+
+    public float currentCustomerPatience = 5f;
+    public float finalBuyingPrice;
+
+ 
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Interview()
+    {
+        
+        
+        currentCustomerPatience -= 1f;
+    }
+
+    void Boast()
+    {
+        
+        currentCustomerPatience -= 1f;
+    }
+
+    void Snackes()
+    {
+        currentCustomerPatience = 5;//TODO
+    }
+
+    void Offer()
+    {
+
+    }
+}
